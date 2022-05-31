25d1eec058520d5791eb2e029a35417fbccd38f1
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2022-05-31 12:26:36 +0300

    Create a static view as the root

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
+  root 'static#index'
   namespace :v1, defaults: { format: 'json' } do
     get 'greetings/index'
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
