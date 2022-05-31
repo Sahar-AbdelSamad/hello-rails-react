ad1fb790db3ead9e59450e56058e2a4411133bb7
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2022-06-01 00:47:24 +0300

    Add Greeting component

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,9 +1,17 @@
 import React from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Greeting from "./Greeting";
 
 class App extends React.Component {
   render () {
     return (
-      <h1> Hello </h1>);
+      <BrowserRouter>
+          <Routes>
+            <Route path="/" element={ <h1> App component </h1> } />
+            <Route path="/greeting" element={ <Greeting/> } />
+          </Routes>
+        </BrowserRouter>
+    );
   }
 }
 
---
+++ app/javascript/components/Greeting.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+class Greeting extends React.Component {
+  render () {
+    return (
+      <h1> Greetings </h1>
+    );
+  }
+}
+
+export default Greeting
