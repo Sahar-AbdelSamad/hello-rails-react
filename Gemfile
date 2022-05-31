205cefb6a2de73635194b2698fb04a607c1b9eac
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2022-05-31 21:41:58 +0300

    Add App component

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby "3.1.2"
 
 gem 'webpacker'
 gem 'react-rails'
+gem 'pg'
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.3"
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root 'static#index'
+
   namespace :v1, defaults: { format: 'json' } do
     get 'greetings/index'
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  get '*page', to: 'static#index', constraints: ->(req) do
+    !req.xhr? && req.format.html?
+  end
 end
