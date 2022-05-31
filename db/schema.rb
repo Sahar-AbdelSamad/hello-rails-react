10a9d29d21356b14f48f216dca5765d62deaf745
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2022-05-31 21:41:04 +0300

    Use postgres

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_05_31_085942) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "greetings", force: :cascade do |t|
     t.string "message"
     t.datetime "created_at", null: false
