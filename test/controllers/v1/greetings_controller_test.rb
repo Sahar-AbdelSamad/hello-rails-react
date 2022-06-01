8c2c509bf2f81fcb8ffbf801b92d6d2c0b532d5c
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2022-06-01 09:45:07 +0300

    Add linters and fix errors

--- test/controllers/v1/greetings_controller_test.rb
+++ test/controllers/v1/greetings_controller_test.rb
@@ -1,7 +1,7 @@
-require "test_helper"
+require 'test_helper'
 
 class V1::GreetingsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get v1_greetings_index_url
     assert_response :success
   end
